bdf8924e5612d15ab9ce764cf9cbc2e2caca5d5a
Raz <retraz123@gmail.com>
2025-02-14 11:56:50 +0200

    add top section to game page

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,26 +1,25 @@
-'use client';
-import { usePlayer } from '@/contsxt/PlayerContext';
-import React from 'react';
+import GameHeader from '@/components/game/GameHeader';
 
 const GamePage = () => {
-    const { playerName } = usePlayer();
 
     return (
-        <div>
-            <div>
-            <h1>Welcome, {playerName || 'Guest'}!</h1>
-            </div>
-            <div>
-                question
-            </div>
-            <div>
-                answers
-            </div>
-            <div>
-                submit
-            </div>
-            <div>
-                progress
+        <div className='flex flex-col items-center'>
+            <div className='w-full sm:w-1/2 md:w-1/3 h-1/2 px-10 rounded-xl'>
+                <div>
+                    <GameHeader />
+                </div>
+                <div>
+                    question
+                </div>
+                <div>
+                    answers
+                </div>
+                <div>
+                    submit
+                </div>
+                <div>
+                    progress
+                </div>
             </div>
         </div>
     );
---
+++ components/game/GameHeader.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { usePlayer } from '@/contsxt/PlayerContext';
+
+const GameHeader = () => {
+    const { playerName } = usePlayer();
+
+  return (
+    <div className='flex justify-between mt-10'>
+        <h1 className='text-2xl font-semibold'>Welcome, {playerName || 'Guest'}!</h1>
+        <p className='text-lg bg-success-200 rounded-lg px-2 flex items-center text-success-700'>dificulty</p>
+    </div>
+  )
+}
+
+export default GameHeader
