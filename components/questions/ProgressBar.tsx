3813b7f9f605c113522aa213c85ee9add3fb2a0d
Raz <retraz123@gmail.com>
2025-02-17 21:28:17 +0200

    use real info from context

--- components/questions/ProgressBar.tsx
+++ components/questions/ProgressBar.tsx
@@ -1,17 +1,27 @@
 'use client';
+
+import { useGame } from '@/contexts/GameContext';
 import { Progress } from '@heroui/react';
-import React from 'react';
 
 const ProgressBar = () => {
+    const { currentQuestionIndex, questions } = useGame();
     return (
-        <div className='flex flex-wrap justify-between'>
-          <div className='text-sm'>
-            Time left: <span className='text-primary font-semibold'>30s</span>
-          </div>
-          <div className='text-sm'>
-            Question 3 of 10
-          </div>
-            <Progress aria-label="Loading..." className="w-full mt-2" value={30} />
+        <div className="flex flex-wrap justify-between">
+            <div className="text-sm">
+                Time left:{' '}
+                <span className="text-primary font-semibold">30s</span>
+            </div>
+            <div className="text-sm">
+                Question {currentQuestionIndex + 1} of {questions.length}
+            </div>
+            <Progress
+                aria-label="Loading..."
+                className="w-full mt-2"
+                value={
+                    (currentQuestionIndex * 100) / questions.length +
+                    100 / questions.length
+                }
+            />
         </div>
     );
 };
