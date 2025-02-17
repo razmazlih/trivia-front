ac8fd72e44780e93e558f87195fd55348dcde5a2
Raz <retraz123@gmail.com>
2025-02-17 20:53:59 +0200

    update question card component

--- components/questions/QuestionCard.tsx
+++ components/questions/QuestionCard.tsx
@@ -1,16 +1,14 @@
 'use client';
 import { Card, CardHeader } from '@heroui/react';
-import React from 'react';
 import Answers from './Answers';
+import { QuestionCardProps } from '@/lib/types';
 
-const QuestionCard = () => {
+const QuestionCard = ({ question }: QuestionCardProps) => {
     return (
         <Card>
             <CardHeader className="select-none min-w-full flex flex-col items-start">
-                <h1 className="text-xl font-bold mt-2">
-                    What the capital of France?
-                </h1>
-                <Answers />
+                <h1 className="text-xl font-bold mt-2">{question.question}</h1>
+                <Answers answers={question.options} />
             </CardHeader>
         </Card>
     );
