56495f84c87c327228c09707f0639e0759c9f5f6
Raz <retraz123@gmail.com>
2025-02-17 20:52:33 +0200

    update main page

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,22 +1,5 @@
-import MainNavbar from '@/components/landing/MainNavbar';
-import NavigateButtons from '@/components/questions/NavigateButtons';
-import ProgressBar from '@/components/questions/ProgressBar';
-import QuestionCard from '@/components/questions/QuestionCard';
-import React from 'react';
+import Game from '@/components/questions/Game';
 
-const GamePage = () => {
-    return (
-        <div className='flex flex-col items-center'>
-            <MainNavbar />
-            <div className='flex flex-col justify-center items-center w-full sm:w-10/12 lg:w-8/12'>
-                <div className="w-10/12 mt-10 sm:mt-24">
-                    <ProgressBar />
-                </div>
-                <div className='w-10/12 mt-8'><QuestionCard /></div>
-                <div className='w-10/12'><NavigateButtons /></div>
-            </div>
-        </div>
-    );
-};
-
-export default GamePage;
+export default function GamePage() {
+    return <Game />;
+}
