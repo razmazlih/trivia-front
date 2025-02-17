95312ce89e691cb29324e5a661ac965c74dcefab
Raz <retraz123@gmail.com>
2025-02-17 20:52:19 +0200

    add context to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,28 @@
+import { GameProvider } from '@/contexts/GameContext';
 import './globals.css';
+import { Question } from '@/lib/types';
+import { BASE_API_URL } from '@/lib/config';
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const res = await fetch(`${BASE_API_URL}/api/questions`, {
+        next: { revalidate: 3600 },
+    });
+    if (!res.ok) {
+        throw new Error('Failed to fetch questions');
+    }
+    const questions: Question[] = await res.json();
+
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <GameProvider initialQuestions={questions}>
+                    {children}
+                </GameProvider>
+            </body>
         </html>
     );
 }
