baa39df420d2946599cdea86d872686533d2d7ba
Raz <retraz123@gmail.com>
2025-02-18 13:24:07 +0200

    add results page

---
+++ app/results/page.tsx
@@ -0,0 +1,11 @@
+import MainNavbar from "@/components/landing/MainNavbar";
+
+const resultPage = () => {
+  return (
+    <div className="flex flex-col items-center">
+        <MainNavbar />
+    </div>
+);
+}
+
+export default resultPage
