c5ca074d5cfa2229b9fae08b7e7c37dbd74ee82d
Raz <retraz123@gmail.com>
2025-02-18 17:26:53 +0200

    add flex wrap

--- components/results/ShareButtons.tsx
+++ components/results/ShareButtons.tsx
@@ -4,7 +4,7 @@ import { Button } from "@heroui/react";
 
 const ShareButtons = () => {
   return (
-    <div className="flex gap-4 flex-wrap">
+    <div className="flex gap-4 flex-wrap justify-center items-center">
         <Button className="text-white" color="primary">Share on Facebook</Button>
         <Button className="text-white" color="primary">Share on Twitter</Button>
         <Button className="text-white" color="primary">Share on Instagram</Button>
