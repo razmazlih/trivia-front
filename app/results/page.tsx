d25cc5e4eb642f628139e0fed5086e7e542d1415
Raz <retraz123@gmail.com>
2025-02-18 13:42:51 +0200

    add Header section in results page

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,9 +1,13 @@
 import MainNavbar from "@/components/landing/MainNavbar";
+import HeaderSection from "@/components/results/HeaderSection";
 
 const resultPage = () => {
   return (
     <div className="flex flex-col items-center">
         <MainNavbar />
+        <div className="mt-10 sm:mt-16">
+        <HeaderSection />
+        </div>
     </div>
 );
 }
